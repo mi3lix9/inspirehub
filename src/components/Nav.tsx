b523a37cea713fa2ddc3b502a7e53bca63160595
Ali Al-Shaikh <mi3lix9@outlook.sa>
2023-12-13 19:41:02 +0000

    add user state in the nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-// import { createSupabaseBrowser } from "../utils/supabase";
 import type { User } from "@supabase/supabase-js";
+import { supabase } from "../utils/supabase";
 
 const navMotion = {
   visible: {
@@ -58,20 +58,18 @@ export default function Nav() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    // const supabase = createSupabaseBrowser();
-    // async function getUser() {
-    //   const {
-    //     data: { user },
-    //   } = await supabase.auth.getUser();
-    //   setUser(user);
-    //   console.log(user);
-    // }
-    // getUser();
+    async function getUser() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+      console.log(user);
+    }
+    getUser();
   }, []);
 
   async function logout() {
-    // const supabase = createSupabaseBrowser();
-    // await supabase.auth.signOut();
+    await supabase.auth.signOut();
     setUser(null);
   }
 
@@ -103,40 +101,39 @@ export default function Nav() {
             className="flex flex-col gap-24 text-lg text-black max-md:gap-12"
             isMobile={true}
           />
-          {user === null ? (
-            <motion.div
-              animate={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -75 }}
-              transition={{ delay: 0.9 }}
-              className="flex flex-col w-64 gap-4"
-            >
-              <motion.a
-                className="flex select-none items-center cursor-pointer justify-center rounded-lg  bg-[#5f7fbf] border-2 border-[#5f7fbf] px-4 py-2 mt-3
+
+          <motion.div
+            animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -75 }}
+            transition={{ delay: 0.9 }}
+            className="flex flex-col w-64 gap-4"
+          >
+            <motion.a
+              className="flex select-none items-center cursor-pointer justify-center rounded-lg  bg-[#5f7fbf] border-2 border-[#5f7fbf] px-4 py-2 mt-3
               text-base font-bold text-white align-middle transition-all duration-700 hover:bg-[#3e60a3] hover:border-[#3e60a3] focus:outline-none shadow-md hover:shadow-xl
               disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                href="login"
-              >
-                Sign in
-              </motion.a>
-              <motion.a
-                className="flex select-none items-center justify-center rounded-lg border-2 border-[#5f7fbf] cursor-pointer shadow-md hover:shadow-xl
+              href="login"
+            >
+              Sign in
+            </motion.a>
+            <motion.a
+              className="flex select-none items-center justify-center rounded-lg border-2 border-[#5f7fbf] cursor-pointer shadow-md hover:shadow-xl
               px-4 py-2 mt-3 text-base font-bold text-[#5f7fbf] align-middle transition-all duration-500
             hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-                href="register"
-              >
-                Sign up
-              </motion.a>
-            </motion.div>
-          ) : (
-            <motion.button
+              href="register"
+            >
+              Sign up
+            </motion.a>
+          </motion.div>
+
+          {/* <motion.button
               className="flex select-none items-center justify-center rounded-lg border-2 border-[#5f7fbf] cursor-pointer shadow-md hover:shadow-xl
         px-4 py-2 mt-3 text-base font-bold text-[#5f7fbf] align-middle transition-all duration-500
       hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
               onClick={logout}
             >
               Logout
-            </motion.button>
-          )}
+            </motion.button> */}
         </motion.div>
       )}
 
