9109d2ebbd49e4c4264cbf58e2790e3651273249
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-14 02:01:42 +0300

    Adjust margins in the nav

--- src/components/UI/ProfileMenu.tsx
+++ src/components/UI/ProfileMenu.tsx
@@ -17,13 +17,13 @@ interface ProfileMenuProps {
 
 const ProfileMenu: React.FC<ProfileMenuProps> = ({ toggled, logout }) => {
   return (
-    <div className={`lg:block ${toggled ? "block" : "hidden"} absolute  mr-10`}>
+    <div className={`lg:block ${toggled ? "block" : "hidden"} absolute  mr-12`}>
       <Menu>
         <MenuHandler>
           <Avatar
             variant="circular"
             alt="Tania Andrew"
-            className="w-[4.5rem] h-[4.5rem] mt-12 rounded-full cursor-pointer"
+            className="w-[4.5rem] h-[4.5rem] mt-10 rounded-full cursor-pointer"
             src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90oy1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
             placeholder={undefined}
           />
